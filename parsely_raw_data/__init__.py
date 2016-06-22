d823ee4d14db241510ea8b7fc5bd43d9e89fb021
Emmett Butler <emmett.butler321@gmail.com>
2016-06-22 15:42:38 -0400

    include metadata in public api

--- parsely_raw_data/__init__.py
+++ parsely_raw_data/__init__.py
@@ -14,9 +14,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 from .event import (Event, SlotInfo, SessionInfo, TimestampInfo, VisitorInfo,
-                    DisplayInfo, SlotsMixin)
+                    DisplayInfo, SlotsMixin, Metadata)
 
 __version__ = '0.0.0'
 
 __all__ = ["Event", "SlotInfo", "SessionInfo", "TimestampInfo", "VisitorInfo",
-           "DisplayInfo", "SlotsMixin"]
+           "DisplayInfo", "SlotsMixin", "Metadata"]
