90c1f3267253be6dc8161ca112178b12c090943e
rachelannelise <annelise.lynch@gmail.com>
2017-10-04 15:20:43 -0400

    first dpl base models

---
+++ dbt/parsely_dpl/models/base/parsely_base_pageviews.sql
@@ -0,0 +1,7 @@
+select * from {{ var('parsely:rawdata') }}
+where action in ('pageview','heartbeat')
+
+-- Questions
+-- where is it best to do where: action = pageview, heartbeat, videostart, vheartbeat?
+-- how/when to handle metadata updates to posts? since that lives for each row on the pageviews and videoviews tables?
+---- maybe have a content dim?
