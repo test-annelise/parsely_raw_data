405567a79cf28f8a75c8793fe268e370fe0e13cd
Dan Blanchard <dan.blanchard@gmail.com>
2017-11-08 09:52:50 -0500

    Bump version to 2.2.3

--- parsely_raw_data/__init__.py
+++ parsely_raw_data/__init__.py
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-__version__ = '2.2.2'
+__version__ = '2.2.3'
 
 from . import bigquery, docgen, redshift, s3, samples, schema, stream, utils
 
